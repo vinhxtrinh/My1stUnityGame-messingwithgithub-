c5d8ea1fb273be03ba25d65d40cd86d7ae6de6c2
vinhxtrinh <34748123+vinhxtrinh@users.noreply.github.com>
2018-08-15 01:05:53 -0500

    Updated or uploaded the project
    Updated or uploaded the project, hope this works lol

---
+++ Begin Unity 1/Assets/PLayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PLayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+   
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit the cube young man ");
+            movement.enabled = false;         
+        }     
+    }
+}
---
+++ Begin Unity 1/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿ using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float fowardForce = 20000f;
+    public float sidewayForce = 500f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0, 0, fowardForce* Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+        else if(Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
+	}
+}
